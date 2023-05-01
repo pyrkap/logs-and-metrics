da727c4a3d15e217ef2f8db05705410239a1cad1
pyrkap <patryk.pyrka@allegro.pl>
2023-05-01 11:58:37 +0200

    Empty project

---
+++ src/main/java/pyrkap/logsandmetrics/LogsAndMetricsApplication.java
@@ -0,0 +1,13 @@
+package pyrkap.logsandmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsAndMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsAndMetricsApplication.class, args);
+	}
+
+}
