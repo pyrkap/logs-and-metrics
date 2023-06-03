6cf94b946a5fa1215a579196736f5ee28db6c630
pyrkap <patryk.pyrka@allegro.pl>
2023-06-03 14:03:01 +0200

    Logstash with JSON parsing

--- src/main/java/pyrkap/logsandmetrics/RequestCachingFilter.java
+++ src/main/java/pyrkap/logsandmetrics/RequestCachingFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,6 +15,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -25,12 +29,24 @@ public class RequestCachingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String authType = request.getAuthType();
+        String headers = readAllHeaders(request).toString();
+        MDC.put("auth-type", authType);
+        MDC.put("headers", headers);
         LOGGER.info("REQUEST\t :" + readRequest(request.getInputStream()));
+        MDC.clear();
         filterChain.doFilter(request, response);
     }
 
     private String readRequest(InputStream inputStream) {
         return new BufferedReader(new InputStreamReader(inputStream))
                 .lines().collect(Collectors.joining("\n"));
+    }
+    
+    private Map<String, String> readAllHeaders(HttpServletRequest request) {
+        List<String> headersNames = new ArrayList<>();
+        request.getHeaderNames().asIterator().forEachRemaining(headersNames::add);
+        return headersNames.stream()
+                .collect(Collectors.toMap(header -> header, request::getHeader));
     }
 }
